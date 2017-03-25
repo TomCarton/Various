697ae7a0667899819f5e3d05970af32ba7cbaf10
Thomas CARTON <TomCarton@users.noreply.github.com>
2017-03-25 12:54:39 +0100

    cleaned indentation a bit

--- simpleSort.c
+++ simpleSort.c
@@ -13,15 +13,15 @@
 // sort an array of unsigned char
 void sort(unsigned char *array, unsigned int size)
 {
-	unsigned char *p = array;
+    unsigned char *p = array;
 
-	unsigned char *inc = (unsigned char *)malloc(256);
-	memset(inc, 0, 256);
+    unsigned char *inc = (unsigned char *)malloc(256);
+    memset(inc, 0, 256);
 	
-	for (unsigned int i = 0; i < size; ++i) ++inc[array[i]];
-	for (unsigned int i = 0; i < 256; ++i) while (inc[i]--) *p++ = i;
-	
-	free(inc);
+    for (unsigned int i = 0; i < size; ++i) ++inc[array[i]];
+    for (unsigned int i = 0; i < 256; ++i) while (inc[i]--) *p++ = i;
+
+    free(inc);
 }
 
 
@@ -29,14 +29,14 @@ void sort(unsigned char *array, unsigned int size)
 
 void display(unsigned char *array, unsigned int size)
 {
-	printf("unsigned char array[] = {");
-	
-	for (unsigned int i = 0; i < size; ++i)
-	{
-		printf("%3d, ", array[i]);
-	}	
+    printf("unsigned char array[] = {");
 
-	printf("};\n");
+    for (unsigned int i = 0; i < size; ++i)
+    {
+	printf("%3d, ", array[i]);
+    }
+
+    printf("};\n");
 }
 
 
@@ -44,12 +44,12 @@ void display(unsigned char *array, unsigned int size)
 
 int main(int argc, char *argv[])
 {
-	unsigned char values[] = { 5, 1, 37, 2, 7, 15, 27, 23, 0, 2, 23, 0, 23, 4, };
-	unsigned size = sizeof(values);
+    unsigned char values[] = { 5, 1, 37, 2, 7, 15, 27, 23, 0, 2, 23, 0, 23, 4, };
+    unsigned size = sizeof(values);
 
-	display(values, size);
+    display(values, size);
 
-	sort(values, size);
+    sort(values, size);
 
-	display(values, size);
+    display(values, size);
 }
